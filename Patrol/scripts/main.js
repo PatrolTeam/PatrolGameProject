de97480cef148ef050cdbd0c3b6e0273a451724e
Spas Kalinov <spaskicha@gmail.com>
2018-05-08 16:40:57 +0300

    improved player jumping - see #36 and #49

--- Patrol/scripts/main.js
+++ Patrol/scripts/main.js
@@ -8,13 +8,15 @@ var restart;
 var groundHeight = 48;
 var isFlying = true;
 
+var jumpFrame = 0;
+
 var bullets = [];
 var upBullets = [];
 
 function startGame() {
     gameArea.start();
 
-    player = new component(113, 48, "resources/images/player/1.png", 10, 120, "image");
+    player = new component(113, 48, "resources/images/player/1.png", 10, 432, "image");
     player.imgArr = ["resources/images/player/1.png", "resources/images/player/2.png", "resources/images/player/3.png", "resources/images/player/4.png"];
 
     score = new component("30px", "Consolas", "white", 380, 40, "text");
@@ -48,10 +50,6 @@ var gameArea = {
             gameArea.keys[e.keyCode] = true;
         });
         window.addEventListener('keyup', function (e) {
-            if (e.keyCode === 32) {
-                accelerateUp(0.1);
-            }
-
             gameArea.keys[e.keyCode] = false;
         });
     },
@@ -114,7 +112,7 @@ function component(width, height, color, x, y, type) {
     };
 
     this.movePlayer = function () {
-
+        
         this.gravitySpeed += this.gravity;
 
         var tempX = this.x + this.speedX;
@@ -122,8 +120,8 @@ function component(width, height, color, x, y, type) {
         
         if (tempX < 0) {
             this.x = 0;
-        } else if (tempX > rightBorder) {
-            this.x = rightBorder;
+        // } else if (tempX > rightBorder) {
+        //     this.x = rightBorder;
         } else {
             this.x += this.speedX;
         }
@@ -151,7 +149,7 @@ function component(width, height, color, x, y, type) {
         if (this.y > rockbottom) {
             this.y = rockbottom;
             isFlying = false;
-
+            this.gravitySpeed = 0;
         }
     }
     
@@ -187,6 +185,10 @@ function updateGameArea() {
         player.image.src = player.imgArr[player.currFrame];
     }
 
+    if (gameArea.frameNo - jumpFrame === 10 && isFlying) {
+        player.gravity = 0.5;
+    }
+
     // manage player-obstacle collision
     for (i = 0; i< myObstacles.length; i += 1) {
         if (player.crashWith(myObstacles[i])) {
@@ -206,19 +208,21 @@ function updateGameArea() {
     groundLine.newPos();
     groundLine.update();
 
-    player.speedX = 0;
-    player.speedY = 0;
+    if (!isFlying) {
+        player.speedX = 0;
+        player.speedY = 0;
+    }
 
     // handle keyboard input
     if (gameArea.keys && gameArea.keys[65]) {moveLeft() }
     if (gameArea.keys && gameArea.keys[68]) {moveRight() }
-    if (gameArea.keys && gameArea.keys[32]) {accelerateUp(-0.2)}
+    if (gameArea.keys && gameArea.keys[32]) {jump()}
     if (gameArea.keys && gameArea.keys[67]) {shoot(); upShoot()}
 
     // spawn obstacles logic
-    if (gameArea.frameNo === 1 || everyinterval(150)) {
-        obstacleHeight = 48;
-        obstacleWidth = 48;
+    if (gameArea.frameNo === 1 || everyinterval(600)) {
+        obstacleHeight = 32;
+        obstacleWidth = 32;
 
         obstacleX = gameArea.canvas.width;
         obstacleY = gameArea.canvas.height - groundHeight - obstacleHeight;
@@ -307,13 +311,12 @@ function moveRight() {
     }
 }
 
-function accelerateUp(n) {
-    var rockbottom = gameArea.canvas.height - player.height - groundHeight;
-    if (player.y == rockbottom) {
+function jump() {
+    if (!isFlying) {
         player.gravity = -1;
+        player.speedX += 3;
         isFlying = true;
-    } else {
-        player.gravity = n;
+        jumpFrame = gameArea.frameNo;
     }
 }
 
