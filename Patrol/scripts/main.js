97d73aee7a842c3d7c2419a8965bfaefb78be2eb
Spas Kalinov <spaskicha@gmail.com>
2018-06-02 18:37:37 +0300

    improved randomization, added a random destination for airships

--- Patrol/scripts/main.js
+++ Patrol/scripts/main.js
@@ -510,7 +510,8 @@ function updateGameArea() {
 
     // spawn obstacles logic
     if (gameArea.frameNo > 60 && everyinterval(400)) {
-        var index = Math.floor((Math.random() * 10)) % enemies.length;
+        // var index = Math.floor((Math.random() * 10)) % enemies.length;
+        var index = randomNumberBetween(0, enemies.length - 1);
         var currObstacle = enemies[index];
 
         var obstacle = new component(currObstacle[0], currObstacle[1], currObstacle[2], 0, 0, "image");
@@ -525,14 +526,18 @@ function updateGameArea() {
         } else if (currObstacle[3] === "airship") {
             enemiesCount++;
 
+            obstacle.x = gameArea.canvas.width;
             obstacle.y = 60;
+
             obstacle.speedX = -1;
+            obstacle.destinationPoint = randomNumberBetween(100, 500);
         } else if (currObstacle[3] === "bomber") {
             enemiesCount++;
 
             obstacle.y = 100;
 
-            let startingPoint = Math.floor((Math.random() * 10) + 1);
+            // let startingPoint = Math.floor((Math.random() * 10) + 1);
+            let startingPoint = randomNumberBetween(1, 10);
             if (startingPoint <= 5) {
                 obstacle.x = 0;
                 obstacle.speedX = 3;
@@ -604,30 +609,24 @@ function updateGameArea() {
             }
 
             if (myObstacles[i].obstacleType === "airship") {
-                if (myObstacles[i].speedX != 0) {
-                    if (myObstacles[i].x <= gameArea.canvas.width / 2) {
-                        myObstacles[i].speedX = 0;
+                let airship = myObstacles[i];
+
+                if (airship.speedX != 0) {
+                    if (airship.x <= airship.destinationPoint) {
+                        airship.speedX = 0;
                     }
                 } else if (everyinterval(150)) {
-                    let bulletX = myObstacles[i].x + myObstacles[i].width / 2 - 10;
-                    let bulletY = myObstacles[i].y + 48;
+                    let bulletX = airship.x + airship.width / 2 - 10;
+                    let bulletY = airship.y + 48;
+
+                    let distanceX = bulletX - (player.x + player.width / 2);
+                    let distanceY = gameArea.canvas.height - bulletY - groundLine.height - player.height;
+                    let ratio = distanceX / distanceY;
 
                     let newBullet = new component(24, 24, "resources/images/objects/bullet4.png", bulletX, bulletY, "image");
 
-                    let distanceX = bulletX - player.x;
-                    if (distanceX > 0) {
-                        distanceX += player.width / 2;
-                    } else {
-                        distanceX -= player.width / 2;
-                    }
-
-                    let distanceY = gameArea.canvas.height - bulletY;
-
-                    let ratio = distanceX / distanceY;
-
                     newBullet.speedY = 1;
                     newBullet.speedX = newBullet.speedY * ratio;
-
 
                     airshipBullets.push(newBullet);
                 }
@@ -916,4 +915,8 @@ function initHighScoreTable() {
         tableCells2.innerText = highScoreArr[i];
 
     }
+}
+
+function randomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
